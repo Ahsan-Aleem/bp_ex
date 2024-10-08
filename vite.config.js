49d7e4b953c544e211107448a8685c2da08052bd
Ahsan Aleem <ahsanaleemofficial@gmail.com>
2024-10-09 03:25:36 +0500

    u

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Change this to 'dist' if you want to keep the default
+  },
   plugins: [react()],
 })
